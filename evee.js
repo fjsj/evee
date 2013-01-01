309c8058bd87af41625ccea94053a6f700351726
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-01-01 18:40:19 -0300

    Testing events template

--- evee.js
+++ evee.js
@@ -1,18 +1,33 @@
 if (Meteor.isClient) {
+  Handlebars.registerHelper('ifIsNull', function(value, options) {
+    if (value === null) {
+      return options.fn(this);
+    } else {
+      return options.inverse(this);
+    }
+  });
+
+  function fetchEvents (accessToken, callback) {
+    var url = "https://graph.facebook.com/acasadocachorropreto/events?fields=id,description,start_time,end_time,location,name,venue,picture.width(100).height(100).type(square)";
+    url += "&access_token=" + accessToken;
+    Meteor.http.get(url, {timeout: 30000}, callback);
+  }
+
   Template.heroUnit.message = function () {
     return "Encontre eventos em Recife.";
   };
   Template.heroUnit.callToAction = function () {
     return "Cadastre-se agora mesmo com o seu Facebook e veja quais são e quem vai para os principais eventos da cidade!";
-  }
+  };
+  Template.morningEvents.events = function () {
+    return Session.get("events") || null;
+  };
   Meteor.startup(function () {
     $('.gmap').mobileGmap();
-    var url = "https://graph.facebook.comacasadocachorropreto/events?fields=id,description,start_time,end_time,location,name,venue,picture.width(100).height(100).type(square)";
-    url += "&access_token=" + fbAccessToken;
-    Meteor.http.get(url, {timeout: 30000}, function(error, result) {
+    fetchEvents(fbAccessToken, function (error, result) {
       if (result.statusCode == 200) {
         var json = JSON.parse(result.content);
-        console.log(json);
+        Session.set("events", json.data)
       } else {
         console.log(error);
       }
