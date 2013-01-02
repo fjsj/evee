1ed6c3f1aaa7eae688c168193e76386711b5a04c
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-01-02 19:30:45 -0300

    Getting events from friends

--- evee.js
+++ evee.js
@@ -8,7 +8,7 @@ if (Meteor.isClient) {
   });
 
   function fetchEvents (accessToken, callback) {
-    var url = "https://graph.facebook.com/acasadocachorropreto/events?fields=id,description,start_time,end_time,location,name,venue,picture.width(100).height(100).type(square),attending.limit(20).fields(name, picture.type(square))";
+    var url = "https://graph.facebook.com/me?fields=friends.fields(events.since(1357162191).limit(25).fields(id,description,start_time,end_time,location,name,venue,picture.width(100).height(100).type(square)))";
     url += "&access_token=" + accessToken;
     Meteor.http.get(url, {timeout: 30000}, callback);
   }
@@ -30,7 +30,25 @@ if (Meteor.isClient) {
     fetchEvents(fbAccessToken, function (error, result) {
       if (result.statusCode == 200) {
         var json = JSON.parse(result.content);
-        Session.set("events", json.data)
+        var eventsIds = {};
+        var events = [];
+        json.friends.data.forEach(function (friend) {
+          if (friend.events) {
+            friend.events.data.forEach(function (event) {
+              if (!(event.id in eventsIds)) {
+                events.push(event);
+                eventsIds[event.id] = true;
+              }
+            });
+          }
+        });
+        events.sort(function (a, b) {
+          return moment(a.start_time).valueOf() - moment(b.start_time).valueOf();
+        });
+        events.forEach(function (event) {
+          event.start_time = moment(event.start_time).format("HH:mm DD/MM/YY");
+        });
+        Session.set("events", events);
       } else {
         console.log(error);
       }
