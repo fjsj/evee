e0bd71a35585a38eed4a79a7c1cab9a0ff53b4a2
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-01-03 01:11:38 -0300

    Day and Night events

--- evee.js
+++ evee.js
@@ -8,7 +8,8 @@ if (Meteor.isClient) {
   });
 
   function fetchEvents (accessToken, callback) {
-    var url = "https://graph.facebook.com/me?fields=friends.fields(events.since(1357162191).limit(25).fields(id,description,start_time,end_time,location,name,venue,picture.width(100).height(100).type(square)))";
+    var timestamp = moment().unix();
+    var url = "https://graph.facebook.com/me?fields=friends.fields(events.since(" + timestamp + ").limit(25).fields(id,description,start_time,end_time,location,name,venue,picture.width(100).height(100).type(square)))";
     url += "&access_token=" + accessToken;
     Meteor.http.get(url, {timeout: 30000}, callback);
   }
@@ -20,10 +21,10 @@ if (Meteor.isClient) {
     return "Cadastre-se agora mesmo com o seu Facebook e veja quais são e quem vai para os principais eventos da cidade!";
   };
   Template.dayEvents.events = function () {
-    return Session.get("events") || null;
+    return Session.get("dayEvents") || null;
   };
   Template.nightEvents.events = function () {
-    return Session.get("events") || null;
+    return Session.get("nightEvents") || null;
   };
   Meteor.startup(function () {
     $('.gmap').mobileGmap();
@@ -42,13 +43,27 @@ if (Meteor.isClient) {
             });
           }
         });
+        
         events.sort(function (a, b) {
           return moment(a.start_time).valueOf() - moment(b.start_time).valueOf();
         });
+        
+        var dayEvents = [];
+        var nightEvents = [];
         events.forEach(function (event) {
-          event.start_time = moment(event.start_time).format("HH:mm DD/MM/YY");
+          var startTime = moment(event.start_time);
+          var startHour = startTime.hours();
+          if (startHour > 6 && startHour < 18) {
+            dayEvents.push(event);
+          } else {
+            nightEvents.push(event);
+          }
+          
+          event.start_time = startTime.format("HH:mm DD/MM/YY");
         });
-        Session.set("events", events);
+
+        Session.set("dayEvents", dayEvents);
+        Session.set("nightEvents", nightEvents);
       } else {
         console.log(error);
       }
