dd34ba76a8dfd5dbb4e6c23496b3556702038b98
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-01-06 20:32:46 -0300

    Return to home on click on brand

--- client/views/menu.js
+++ client/views/menu.js
@@ -1,4 +1,8 @@
 Template.menu.events({
+  "click .brand": function (ev) {
+    selectedEvent.clear();
+    ev.preventDefault();
+  },
   "click .back-home": function (ev) {
     selectedEvent.clear();
     ev.preventDefault();
