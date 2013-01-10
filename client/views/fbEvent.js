4efc3280048fbcb8acbfaf1b3cc0f5e77837171c
Flávio Juvenal <flaviojuvenal@gmail.com>
2013-01-10 20:45:02 -0300

    More precise male and female calculation

--- client/views/fbEvent.js
+++ client/views/fbEvent.js
@@ -9,12 +9,14 @@ Template.fbEvent.attendees = function () {
 Template.fbEvent.maleRatio = function () {
   var attendees = facebook.getEventAttendees(this.id);
   if (attendees && attendees.length) {
-    var total = attendees.length;
-    var males = _.reduce(attendees, function (memo, a) { return memo + (a.gender === "male" ? 1 : 0) }, 0);
-    return Math.floor((males / total) * 100);
-  } else {
-    return null;
+    var attendees = _.filter(attendees, function (a) { return a.hasOwnProperty("gender") });
+    if (attendees.length) {
+      var total = attendees.length;
+      var males = _.reduce(attendees, function (memo, a) { return memo + (a.gender === "male" ? 1 : 0) }, 0);
+      return Math.floor((males / total) * 100);
+    }
   }
+  return null;
 };
 
 Template.fbEvent.helpers({
